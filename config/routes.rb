e6cd0e8ce8e7442421b26b5a85c177b6b451ae70
Christian Carijutan <christiancarijutan@Christian-Carijutan.local>
2022-03-10 14:34:43 +0800

    Crud of application

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :users
     resources :leave_credits
+    resources :leave_applications
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
