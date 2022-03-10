01352097abee042da97275c78893803cf536103b
dadia-willand@c-fo.com <dadia-willand@c-fo.com>
2022-03-10 13:47:48 +0800

    add login method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :users
+    post '/users/login', to: 'users#login' 
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
